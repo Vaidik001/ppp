b9ea7844eedefde0cabf8bc6346a7c4f1f972da2
Vaidik001 <vaidikpatel1932@gmail.com>
2024-05-20 16:51:10 -0600

    python function 01

---
+++ first.py
@@ -0,0 +1,28 @@
+#print("hello world")
+
+#a=10;
+#b=20;
+#print("enter subject marks out of 100")
+#a=float(input("enter java marks:"))
+#b=float(input("enter dbms marks:"))
+#c=float(input("enter python marks:"))
+#print("java=",a)
+#print("dbms=",b)
+#print("python=",c)
+#total=a+b+c
+#print("total marks=",total)
+#par=int(total*100/300)
+#print("par=","%",par)
+
+a=int(input("enter no1:"))
+b=int(input("enter no2:"))
+c=int(input("enter no3:"))
+
+if(a>b and a>c):
+    print("a is greater")
+
+    elif b>c
+    print("b is greter")
+
+    if a>c
+    print("c is greater")
