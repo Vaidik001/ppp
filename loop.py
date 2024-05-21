49f037a46505107769a7d67447804bb221ac5a53
Vaidik001 <vaidikpatel1932@gmail.com>
2024-05-21 11:03:51 -0600

    python programs

---
+++ loop.py
@@ -0,0 +1,22 @@
+#for loop 
+'''
+for x in range(10,0,-1):
+    print (x)
+'''
+#while loop 
+'''
+no=int(input("enter no :"))
+while no >= 0:
+    print(no)
+    no -= 1 
+'''
+"""
+no=int(input("enter no:"))
+rev=0
+while no>0:
+    rem=no%10
+    no=no//10
+    rev=rev*10+rem
+print("rev is :",rev)
+
+"""
